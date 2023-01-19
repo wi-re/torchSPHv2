8cc920756e6d315c7b07a1224cee88eda8ecea62
Rene <rene.winchenbach@tum.de>
2023-01-19 15:19:02 +0100

    Some shifting works maybe:tm:

---
+++ tt.py
@@ -0,0 +1,3 @@
+(-315*torch.sin(2*c)*d**7-210*torch.sin(2*c)*d**5)*torch.log(torch.sqrt(1-d**2)+1)
+(315*torch.sin(2*c)*d**7+210*torch.sin(2*c)*d**5)*torch.log(d)
+torch.sqrt(d**2-1)*(128.j*torch.sin(2*c)*d**7+421.j*torch.sin(2*c)*d**5-36.j*torch.sin(2*c)*d**3+12.j*torch.sin(2*c)*d)
